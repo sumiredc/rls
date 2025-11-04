8104f536fa9f82f8da36424c9ebf58e1da2c4b60
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-11-05 01:29:13 +0900

    cli 1.4 tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,23 @@
-// use clap::Parser;
+use anyhow::{Context, Result};
+use clap::Parser;
 
-// #[derive(Parser)]
-// struct Cli {
-//     pattern: String,
-//     path: std::path::PathBuf,
-// }
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
 
-#[derive(Debug)]
-struct CustomError(String);
+fn main() -> Result<()> {
+    let args = Cli::parse();
 
-fn main() -> Result<(), CustomError> {
-    // let args = Cli::parse();
-    // let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
-    // for line in content.lines() {
-    //     if line.contains(&args.pattern) {
-    //         println!("{}", line);
-    //     }
-    // }
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 
-    let path = "test.txt";
-    let content = std::fs::read_to_string(path)
-        .map_err(|err| CustomError(format!("Error reading `{}`: {}", path, err)))?;
-    println!("file content: {}", content);
     Ok(())
 }
