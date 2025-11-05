49f288c6029f6c0836b4cfe5ab8897068948a3b1
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-11-05 20:22:36 +0900

    output file lists

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,10 @@
-use anyhow::Result;
-use crossbeam_channel::{Receiver, bounded, select, tick};
-use std::time::Duration;
+use std::{fs, io};
 
-fn ctrl_channel() -> Result<Receiver<()>, ctrlc::Error> {
-    let (sender, receiver) = bounded(100);
-    ctrlc::set_handler(move || {
-        let _ = sender.send(());
-    })?;
+fn main() -> io::Result<()> {
+    let entries = fs::read_dir(".")?;
 
-    Ok(receiver)
-}
-
-fn main() -> Result<()> {
-    let ctrl_c_events = ctrl_channel()?;
-    let ticks = tick(Duration::from_secs(1));
-
-    loop {
-        select! {
-            recv(ticks) -> _ => {
-                println!("working!");
-            }
-            recv(ctrl_c_events) -> _ => {
-                println!();
-                println!("Goodbye!");
-                break;
-            }
-        }
+    for entry in entries {
+        println!("{}", entry?.file_name().to_string_lossy());
     }
 
     Ok(())
