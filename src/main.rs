43033c1173e33c551b20019ffa46eafead607f66
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-11-05 23:51:40 +0900

    args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
+use clap::Parser;
+use std::path::PathBuf;
 use std::{fs, io};
 
+#[derive(Parser)]
+struct Cli {
+    path: Option<PathBuf>,
+}
+
 fn main() -> io::Result<()> {
-    let entries = fs::read_dir(".")?;
+    let args = Cli::parse();
+    let path = match args.path {
+        Some(p) => p,
+        None => PathBuf::new().join("."),
+    };
+
+    let entries = fs::read_dir(path)?;
 
     for entry in entries {
         println!("{}", entry?.file_name().to_string_lossy());
