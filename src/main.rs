965ea0fd45b668277f1715e096c0f2b37aed7f45
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-11-06 01:12:20 +0900

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
+use anyhow::{Context, Result};
 use clap::Parser;
+use std::fs;
 use std::path::PathBuf;
-use std::{fs, io};
 
 #[derive(Parser)]
 struct Cli {
     path: Option<PathBuf>,
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
     let args = Cli::parse();
     let path = match args.path {
         Some(p) => p,
         None => PathBuf::new().join("."),
     };
 
-    let entries = fs::read_dir(path)?;
+    let entries =
+        fs::read_dir(&path).context(format!("Failed to read directory '{}'", path.display()))?;
 
-    for entry in entries {
-        println!("{}", entry?.file_name().to_string_lossy());
+    for e in entries {
+        let entry = e.context("Failed to read a directory entry")?;
+        println!("{}", entry.file_name().to_string_lossy());
     }
 
     Ok(())
